0fa053fe77eba364505c7540de6d55e3e3507d62
David Maxson <scnerd@gmail.com>
2015-06-11 14:32:48 -0700

    Improvements to BackgroundGenerator, addition of BitmapHelpers

--- BackgroundGenerator.cs
+++ BackgroundGenerator.cs
@@ -3,58 +3,117 @@ using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Helpers
 {
-    public class BackgroundGenerator<T> : IEnumerator<T>
+    public class BackgroundGenerator<T> : Task, IEnumerator<T>
     {
         bool IsPopped = false;
         T PoppedCurrent = default(T);
-        ConcurrentQueue<T> SavedItems = new ConcurrentQueue<T>();
-        Thread ComputeItems;
-        int MaxCache;
+        ConcurrentQueue<T> SavedItems;
+        CancellationTokenSource Canceller;
         bool Enabled = false;
 
-        public BackgroundGenerator(IEnumerator<T> Generator, int MaxCache = -1, bool AutoStart = true)
-            : this(() => {Generator.MoveNext(); return Generator.Current;}, MaxCache, AutoStart)
-        { }
-
-        public BackgroundGenerator(Func<T> GenerateNext, int MaxCache = -1, bool AutoStart = true)
+        private BackgroundGenerator(Action Looper, CancellationTokenSource Canceller, ConcurrentQueue<T> Queue, bool AutoStart = true)
+            : base(Looper, Canceller.Token)
         {
-            this.MaxCache = MaxCache;
-            ComputeItems = new Thread(() =>
-                {
-                    try
-                    {
-                        while (Enabled && ComputeItems.IsAlive)
-                        {
-                            lock(SavedItems)
-                            {
-                                if (SavedItems.Count == MaxCache)
-                                    Monitor.Wait(SavedItems);
-                            }
-                            T result = GenerateNext();
-                            lock (SavedItems)
-                            {
-                                SavedItems.Enqueue(result);
-                                Monitor.Pulse(SavedItems);
-                            }
-                        }
-                    }
-                    catch (InvalidOperationException) { }
-                });
-            ComputeItems.IsBackground = true;
+            this.SavedItems = Queue;
+            this.Canceller = Canceller;
             if (AutoStart)
                 this.Start();
         }
 
-        public static BackgroundGenerator<T> CreateWithCarryover<R>(Func<R, Tuple<T, R>> CarryoverGenerateNext, int MaxCache = -1, bool AutoStart = true)
+        private BackgroundGenerator(IEnumerable<T> Precomputed)
+            : base(() => { })
+        {
+            SavedItems = new ConcurrentQueue<T>(Precomputed);
+        }
+
+        public  static BackgroundGenerator<T> FromEnumerable(IEnumerable<T> Precomputed)
+        {
+            return new BackgroundGenerator<T>(Precomputed);
+        }
+
+        public static BackgroundGenerator<T> FromEnumerator(IEnumerator<T> Generator, int MaxCache = -1,
+            bool AutoStart = true)
+        {
+            return BackgroundGenerator<T>.FromFunc(() =>
+            {
+                Generator.MoveNext();
+                return Generator.Current;
+            }, MaxCache, AutoStart);
+        }
+
+        public static BackgroundGenerator<T> FromFunc(Func<T> GenerateNext, int MaxCache = -1, bool AutoStart = true)
+        {
+            ConcurrentQueue<T> Queue = new ConcurrentQueue<T>();
+            CancellationTokenSource Canceller = new CancellationTokenSource();
+            return new BackgroundGenerator<T>(() =>
+            {
+                try
+                {
+                    while (!Canceller.Token.IsCancellationRequested)
+                    {
+                        Monitor.Enter(Queue);
+                        if (Queue.Count == MaxCache)
+                            Monitor.Wait(Queue);
+                        Monitor.Exit(Queue);
+                        T result = GenerateNext();
+                        Monitor.Enter(Queue);
+                        Queue.Enqueue(result);
+                        Monitor.Pulse(Queue);
+                        Monitor.Exit(Queue);
+                    }
+                }
+                catch (InvalidOperationException)
+                {
+                }
+                catch (OperationCanceledException)
+                {
+                }
+                finally { Monitor.Exit(Queue); }
+            }, Canceller, Queue, AutoStart);
+        }
+
+        public static BackgroundGenerator<T> Chain<R>(Func<R, T> GenerateNext, IEnumerator<R> Source, int MaxCache = -1,
+            bool AutoStart = true)
+        {
+            return BackgroundGenerator<T>.FromFunc(() =>
+            {
+                if (Source.MoveNext())
+                {
+                    return GenerateNext(Source.Current);
+                }
+                else
+                {
+                    throw new OperationCanceledException("End of background generator reached");
+                }
+            }, MaxCache, AutoStart);
+        }
+
+        public BackgroundGenerator<R> Chain<R>(Func<T, R> GenerateNext, int MaxCache = -1, bool AutoStart = true)
+        {
+            return BackgroundGenerator<R>.FromFunc(() =>
+            {
+                if (this.MoveNext())
+                {
+                    return GenerateNext(this.Current);
+                }
+                else
+                {
+                    throw new OperationCanceledException("End of background generator reached");
+                }
+            }, MaxCache, AutoStart);
+        } 
+
+        public static BackgroundGenerator<T> FromFuncWithCarryover<R>(Func<R, Tuple<T, R>> CarryoverGenerateNext, int MaxCache = -1, bool AutoStart = true)
         {
             R ClosureCarryover = default(R);
-            return new BackgroundGenerator<T>(() =>
+            return BackgroundGenerator<T>.FromFunc(() =>
             {
                 var tup = CarryoverGenerateNext(ClosureCarryover);
                 ClosureCarryover = tup.Item2;
@@ -88,15 +147,14 @@ namespace Helpers
             lock (_MoveNextSync)
             {
                 bool Succeeded;
-                lock (SavedItems)
-                {
-                    if (SavedItems.Count == 0)
-                        Monitor.Wait(SavedItems);
-                    T Result;
-                    IsPopped = Succeeded = SavedItems.TryDequeue(out Result);
-                    Monitor.Pulse(SavedItems);
-                    PoppedCurrent = Result;
-                }
+                Monitor.Enter(SavedItems);
+                if (Monitor.IsEntered(SavedItems) && SavedItems.Count == 0)
+                    Monitor.Wait(SavedItems);
+                T Result;
+                IsPopped = Succeeded = SavedItems.TryDequeue(out Result);
+                PoppedCurrent = Result;
+                Monitor.Pulse(SavedItems);
+                Monitor.Exit(SavedItems);
                 return Succeeded;
             }
         }
@@ -108,15 +166,9 @@ namespace Helpers
             IsPopped = false;
         }
 
-        public void Start()
+        public IEnumerator GetEnumerator()
         {
-            Enabled = true;
-            ComputeItems.Start();
-        }
-
-        public void Stop()
-        {
-            Enabled = false;
+            return this;
         }
     }
 }
